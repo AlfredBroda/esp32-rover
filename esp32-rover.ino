ed7e5f8b42639059a1d440a5494265aa493bcc26
Alfred Broda <alfred.broda@gmail.com>
2023-05-20 11:09:20 +0200

    Add PlatformIO config and DSHOT motor control

--- esp32-rover.ino
+++ esp32-rover.ino
@@ -1,7 +1,7 @@
 /*********
   Based on ESP32-cam code by Rui Santos
   Complete instructions at https://RandomNerdTutorials.com/esp32-cam-projects-ebook/
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 *********/
@@ -9,25 +9,39 @@
 #include <WiFi.h>
 #include "esp_timer.h"
 #include "Arduino.h"
-#include "soc/soc.h"             // disable brownout problems
-#include "soc/rtc_cntl_reg.h"    // disable brownout problems
+#include "soc/soc.h"          // disable brownout problems
+#include "soc/rtc_cntl_reg.h" // disable brownout problems
 #include "esp_http_server.h"
+#include "DShotRMT.h"
 
 // Replace with your network credentials
-const char* ssid = "SSI";
-const char* password = "PASS";
+const char *ssid = "koza";
+const char *password = "123dieg0!";
 
-#define MOTOR_1_PIN_1    14
-#define MOTOR_1_PIN_2    27
-#define MOTOR_2_PIN_1    13
-#define MOTOR_2_PIN_2    12
+// Define the GPIO pin connected to the motor and the DShot protocol used
+const int CUTTER_PIN = 2;
+const dshot_mode_t DSHOT_MODE = DSHOT600;
+
+// Initialize a DShotRMT object for the motor
+DShotRMT cutter(CUTTER_PIN, RMT_CHANNEL_0);
+
+// Define the failsafe and initial throttle values
+const int FAILSAFE_THROTTLE = 300;
+const int INITIAL_THROTTLE = 48;
+const int NORMAL_THROTTLE = 500;
+
+#define MOTOR_1_PIN_1 14
+#define MOTOR_1_PIN_2 27
+#define MOTOR_2_PIN_1 13
+#define MOTOR_2_PIN_2 12
 
 httpd_handle_t esp_httpd = NULL;
 
 static const char PROGMEM INDEX_HTML[] = R"rawliteral(
+<!DOCTYPE html>
 <html>
   <head>
-    <title>ESP32 Robot</title>
+    <title>ESP32 Rover</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
       body { font-family: Arial; text-align: center; margin:0px auto; padding-top: 30px;}
@@ -59,11 +73,12 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
     </style>
   </head>
   <body>
-    <h1>ESP32 Robot</h1>
+    <h1>ESP32 Rover</h1>
     <table>
-      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('forward');" ontouchstart="toggleCheckbox('forward');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Forward</button></td></tr>
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('forward');" ontouchstart="toggleCheckbox('forward');">Forward</button></td></tr>
       <tr><td align="center"><button class="button" onmousedown="toggleCheckbox('left');" ontouchstart="toggleCheckbox('left');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Left</button></td><td align="center"><button class="button" onmousedown="toggleCheckbox('stop');" ontouchstart="toggleCheckbox('stop');">Stop</button></td><td align="center"><button class="button" onmousedown="toggleCheckbox('right');" ontouchstart="toggleCheckbox('right');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Right</button></td></tr>
-      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('backward');" ontouchstart="toggleCheckbox('backward');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Backward</button></td></tr>                   
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('backward');" ontouchstart="toggleCheckbox('backward');">Backward</button></td></tr>
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('cutter');" ontouchstart="toggleCheckbox('cutter');" onmouseup="toggleCheckbox('cutter_stop');">Cutter</button></td></tr>
     </table>
    <script>
    function toggleCheckbox(x) {
@@ -76,83 +91,115 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
 </html>
 )rawliteral";
 
-static esp_err_t index_handler(httpd_req_t *req){
+static esp_err_t index_handler(httpd_req_t *req)
+{
   httpd_resp_set_type(req, "text/html");
   return httpd_resp_send(req, (const char *)INDEX_HTML, strlen(INDEX_HTML));
 }
 
-static esp_err_t cmd_handler(httpd_req_t *req){
-  char*  buf;
+static esp_err_t cmd_handler(httpd_req_t *req)
+{
+  char *buf;
   size_t buf_len;
-  char variable[32] = {0,};
-  
+  char variable[32] = {
+      0,
+  };
+
   buf_len = httpd_req_get_url_query_len(req) + 1;
-  if (buf_len > 1) {
-    buf = (char*)malloc(buf_len);
-    if(!buf){
+  if (buf_len > 1)
+  {
+    buf = (char *)malloc(buf_len);
+    if (!buf)
+    {
       httpd_resp_send_500(req);
       return ESP_FAIL;
     }
-    if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
-      if (httpd_query_key_value(buf, "go", variable, sizeof(variable)) == ESP_OK) {
-      } else {
+    if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK)
+    {
+      if (httpd_query_key_value(buf, "go", variable, sizeof(variable)) == ESP_OK)
+      {
+      }
+      else
+      {
         free(buf);
         httpd_resp_send_404(req);
         return ESP_FAIL;
       }
-    } else {
+    }
+    else
+    {
       free(buf);
       httpd_resp_send_404(req);
       return ESP_FAIL;
     }
     free(buf);
-  } else {
+  }
+  else
+  {
     httpd_resp_send_404(req);
     return ESP_FAIL;
   }
 
   int res = 0;
-  
-  if(!strcmp(variable, "forward")) {
+
+  if (!strcmp(variable, "forward"))
+  {
     Serial.println("Forward");
     digitalWrite(MOTOR_1_PIN_1, 1);
     digitalWrite(MOTOR_1_PIN_2, 0);
     digitalWrite(MOTOR_2_PIN_1, 1);
     digitalWrite(MOTOR_2_PIN_2, 0);
   }
-  else if(!strcmp(variable, "left")) {
+  else if (!strcmp(variable, "left"))
+  {
     Serial.println("Left");
     digitalWrite(MOTOR_1_PIN_1, 0);
     digitalWrite(MOTOR_1_PIN_2, 1);
     digitalWrite(MOTOR_2_PIN_1, 1);
     digitalWrite(MOTOR_2_PIN_2, 0);
   }
-  else if(!strcmp(variable, "right")) {
+  else if (!strcmp(variable, "right"))
+  {
     Serial.println("Right");
     digitalWrite(MOTOR_1_PIN_1, 1);
     digitalWrite(MOTOR_1_PIN_2, 0);
     digitalWrite(MOTOR_2_PIN_1, 0);
     digitalWrite(MOTOR_2_PIN_2, 1);
   }
-  else if(!strcmp(variable, "backward")) {
+  else if (!strcmp(variable, "backward"))
+  {
     Serial.println("Backward");
     digitalWrite(MOTOR_1_PIN_1, 0);
     digitalWrite(MOTOR_1_PIN_2, 1);
     digitalWrite(MOTOR_2_PIN_1, 0);
     digitalWrite(MOTOR_2_PIN_2, 1);
   }
-  else if(!strcmp(variable, "stop")) {
+  else if (!strcmp(variable, "stop"))
+  {
     Serial.println("Stop");
     digitalWrite(MOTOR_1_PIN_1, 0);
     digitalWrite(MOTOR_1_PIN_2, 0);
     digitalWrite(MOTOR_2_PIN_1, 0);
     digitalWrite(MOTOR_2_PIN_2, 0);
   }
-  else {
+  else if (!strcmp(variable, "cutter"))
+  {
+    Serial.println("Cutter");
+    // auto throttle_value = (throttle_input > 0) ? throttle_input : FAILSAFE_THROTTLE;
+    cutter.sendThrottleValue(NORMAL_THROTTLE);
+  }
+  else if (!strcmp(variable, "cutter_stop"))
+  {
+    Serial.println("Cutter stop");
+    cutter.sendThrottleValue(INITIAL_THROTTLE);
+  }
+  else
+  {
     res = -1;
   }
 
-  if(res){
+  if (res)
+  {
     return httpd_resp_send_500(req);
   }
 
@@ -160,55 +207,63 @@ static esp_err_t cmd_handler(httpd_req_t *req){
   return httpd_resp_send(req, NULL, 0);
 }
 
-void startServer(){
+void startServer()
+{
   httpd_config_t config = HTTPD_DEFAULT_CONFIG();
   config.server_port = 80;
   httpd_uri_t index_uri = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = index_handler,
-    .user_ctx  = NULL
-  };
+      .uri = "/",
+      .method = HTTP_GET,
+      .handler = index_handler,
+      .user_ctx = NULL};
 
   httpd_uri_t cmd_uri = {
-    .uri       = "/action",
-    .method    = HTTP_GET,
-    .handler   = cmd_handler,
-    .user_ctx  = NULL
-  };
-  if (httpd_start(&esp_httpd, &config) == ESP_OK) {
+      .uri = "/action",
+      .method = HTTP_GET,
+      .handler = cmd_handler,
+      .user_ctx = NULL};
+  if (httpd_start(&esp_httpd, &config) == ESP_OK)
+  {
     httpd_register_uri_handler(esp_httpd, &index_uri);
     httpd_register_uri_handler(esp_httpd, &cmd_uri);
   }
 }
 
-void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
-  
+void setup()
+{
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
+
+  // setup drive motors
   pinMode(MOTOR_1_PIN_1, OUTPUT);
   pinMode(MOTOR_1_PIN_2, OUTPUT);
   pinMode(MOTOR_2_PIN_1, OUTPUT);
   pinMode(MOTOR_2_PIN_2, OUTPUT);
-  
+
   Serial.begin(115200);
   Serial.setDebugOutput(false);
-  
+
   // Wi-Fi connection
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  
-  Serial.print("Robot Ready! Go to: http://");
+
+  // Start generating DShot signal for the motor
+  cutter.begin(DSHOT_MODE, false);
+  Serial.println("DSHOT signal started");
+  cutter.sendThrottleValue(INITIAL_THROTTLE);
+
+  Serial.print("Rover Ready! Go to: http://");
   Serial.println(WiFi.localIP());
-  
+
   // Start streaming web server
   startServer();
 }
 
-void loop() {
-  
+void loop()
+{
 }
